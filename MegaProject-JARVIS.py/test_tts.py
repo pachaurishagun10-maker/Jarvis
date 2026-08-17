7dc1748ccffda8cfe8a71479f067f37312ae7453
Shagun Pachauri <pachaurishagun10@gmail.com>
2026-08-17 16:15:45 +0530

    Updated code

--- MegaProject-JARVIS.py/test_tts.py
+++ MegaProject-JARVIS.py/test_tts.py
@@ -6,5 +6,9 @@ try:
     import pyttsx3
     engine=pyttsx3.init('sapi5')
     engine.setProperty('rate',175)
-    engine.setproperty('volume',1.0)
-    engine.say
+    engine.setProperty('volume',1.0)
+    engine.say("Testing pyttsx3 voice input.Can you hear me?")
+    engine.runAndWait()
+    print("pyttsx3 completed (did you hear audio?)")
+except Exception as e:
+    print(f"pyttsx3 failed: {e}")
